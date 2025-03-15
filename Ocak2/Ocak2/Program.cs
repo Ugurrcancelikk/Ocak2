eac5e1691476355f60568ebbc9c01cca8f625026
TuninRylen <ugurrcancelikk@gmail.com>
2025-03-15 13:28:53 +0300

    v1

---
+++ Ocak2/Ocak2/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Plase enter your first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Plase enter your first number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int total = num1 + num2;
+
+Console.WriteLine("Total: " + total);
